aa551bf0d77754fe8972f64717874950a84e0d46
shaad43 <77321217+shaad43@users.noreply.github.com>
2021-03-16 09:42:30 +0530

    Add files via upload

---
+++ Supply_Mission-main/Package.js
@@ -0,0 +1,16 @@
+class Package{
+  constructor(x,y,width,height){
+    var options = {
+      restitution : 1.43,
+      friction : 1.43 ,
+
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display(){
+    rect(this.body.position.x, this.body.position.y, this.width, this.height );
+  }
+}
